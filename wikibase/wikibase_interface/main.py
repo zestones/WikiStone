b71ebceb70aba3c1c6f689f2d84be5852977bed5
Idriss <idrissbenguezzou@gmail.com>
2023-02-13 21:26:22 +0100

    feat: add index style

--- wikibase/wikibase_interface/main.py
+++ wikibase/wikibase_interface/main.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# @app.route('/search', methods=['POST'])
-# def search():
-#     query = request.form['query']
-#     results = search_wikibase(query)
-#     return render_template('results.html', results=results)
+
+@app.route('/search', methods=['POST'])
+def search():
+    print("recherche")
+   
+    # query = request.form['query']
+    # results = search_wikibase(query)
+    return render_template('results.html', results=results)
