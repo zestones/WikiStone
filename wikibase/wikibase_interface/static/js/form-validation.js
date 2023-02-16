856acae381521a7fba9295846fc1d88db496e0da
Idriss <idrissbenguezzou@gmail.com>
2023-02-16 21:43:34 +0100

    feat: change style search

--- wikibase/wikibase_interface/static/js/form-validation.js
+++ wikibase/wikibase_interface/static/js/form-validation.js
@@ -1,5 +1,5 @@
 function validateForm() {
-    var query = document.getElementById('input-field').value
+    var query = document.getElementById('input-keyword').value
     var errorMessage = document.getElementById("error-message");
 
     if (query == "") {
@@ -11,3 +11,14 @@ function validateForm() {
     errorMessage.style.display = "none";
     return true;
 }
+
+// Add event listener to "open-search" button
+const openSearchBtn = document.querySelector('.open-search');
+const container = document.querySelector('.container');
+const searchOptions = document.querySelector('.search-options');
+
+// Toogle the container and display search options
+openSearchBtn.addEventListener('click', () => {
+    searchOptions.classList.toggle('visible');
+        container.classList.toggle('expanded');
+});
