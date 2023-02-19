08529e3b4f597f86e00e6ac5ae82e50f264c24fd
Idriss <idrissbenguezzou@gmail.com>
2023-02-19 02:04:30 +0100

    feat: display results search of SPARQL query

--- wikibase/wikibase_interface/static/js/wiki-sparql-search.js
+++ wikibase/wikibase_interface/static/js/wiki-sparql-search.js
@@ -4,29 +4,25 @@ class SPARQLQueryDispatcher {
     }
 
     construct(formData) {
-        const query = formData.get('query');
-        const city = formData.get('city');
-        const postalCode = formData.get('postal-code');
-        const region = formData.get('region');
-
-
         let sparqlQuery = `SELECT ?item ?itemLabel ?itemDescription WHERE {`;
         const conditions = [];
 
-        if (city) conditions.push(`?item wdt:P3 "${city}".`);
-        if (postalCode) conditions.push(`?item wdt:P4 "${postalCode}".`);
-        if (region) conditions.push(`?item wdt:P6 "${region}".`);
+        if (formData.city) conditions.push(`?item wdt:P3 "${formData.city}".`);
+        if (formData.postalCode) conditions.push(`?item wdt:P4 "${formData.postalCode}".`);
+        if (formData.region) conditions.push(`?item wdt:P6 "${formData.region}".`);
 
-        if (query) {
+        if (formData.query) {
             sparqlQuery += `?item rdfs:label ?itemLabel.
-            FILTER(REGEX(?itemLabel, "${query}", "i")).`;
+            FILTER(REGEX(?itemLabel, "${formData.query}", "i")).`;
         }
 
         sparqlQuery += conditions.join(' ') + `
             SERVICE wikibase:label {
                 bd:serviceParam wikibase:language "en".
             }
-        }`;
+        }
+        LIMIT 50
+        `;
 
         return sparqlQuery;
     }
@@ -41,6 +37,9 @@ class SPARQLQueryDispatcher {
     parse(data) {
         const items = {};
 
+        if (data.results.bindings.length === 0)
+            return items;
+
         data.results.bindings.forEach((binding) => {
             const id = binding?.item?.value.split("/").pop();
             const label = binding?.itemLabel?.value;
@@ -48,8 +47,79 @@ class SPARQLQueryDispatcher {
             items[id] = { label, description };
         });
 
-        console.log(items)
+        return items;
     }
+}
+
+function getFormData() {
+    const formData = new FormData(form);
+    return {
+        query: formData.get('query'),
+        city: formData.get('city'),
+        postalCode: formData.get('postal-code'),
+        region: formData.get('region')
+    };
+}
+
+function displaySearch(results) {
+    const resultSection = document.querySelector('.result-section');
+    resultSection.innerHTML = '';
+
+    if (Object.keys(results).length === 0) resultSection.innerHTML = '<h2 class="no-result">No data found.</h2>';
+    else {
+        let count = 0;
+        Object.entries(results).forEach(([_, value]) => {
+            if (count >= 7) return;
+
+            const card = document.createElement('div');
+            card.classList.add('card', 'animation-start');
+
+            const title = document.createElement('h2');
+            title.textContent = value?.label;
+            card.appendChild(title);
+
+            if (value.description) {
+                const description = document.createElement('p');
+                description.textContent = value?.description;
+                card.appendChild(description);
+            }
+
+            resultSection.appendChild(card);
+            count++;
+        });
+
+        if (Object.keys(results).length > 7) {
+            const seeMoreBtn = document.createElement('button');
+            seeMoreBtn.textContent = 'See More';
+            seeMoreBtn.classList.add('see-more');
+            resultSection.appendChild(seeMoreBtn);
+        }
+    }
+
+    const cards = document.querySelectorAll('.result-section .card.animation-start');
+    cards.forEach(card => {
+        card.addEventListener('animationend', () => {
+            setTimeout(() => {
+                card.classList.add('animation-done');
+            }, 0.2);
+        });
+    });
+}
+
+
+function validateForm(formData) {
+
+    if (!formData.query && !formData.city && !formData.postalCode && !formData.region) {
+        const errorMessage = document.getElementById('error-message');
+        errorMessage.innerHTML = 'Please provide at least one search criteria.';
+        errorMessage.style.display = 'block';
+        return false;
+    }
+
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.style.display = 'none';
+
+    return true;
 }
 
 // Get the form element
@@ -63,10 +133,28 @@ form.addEventListener('submit', event => {
     const queryDispatcher = new SPARQLQueryDispatcher();
 
     // Get the form data
-    const formData = new FormData(form);
-    
+    formData = getFormData()
+    if (!validateForm(formData)) return;
+
+    // clear the search input fields
+    form.reset(); 
+
     // Construct the SPARQL query
     sparqlQuery = queryDispatcher.construct(formData);
+
     // Fetch and Parse the data
-    queryDispatcher.query(sparqlQuery).then(queryDispatcher.parse);
+    queryDispatcher.query(sparqlQuery)
+        .then(data => {
+            // Collapse the search options
+            searchOptions.classList.remove('visible');
+            container.classList.remove('expanded');
+
+            // Pass the parsed data to the displaySearch function
+            displaySearch(queryDispatcher.parse(data));
+        })
+        .catch(error => {
+            console.error(error);
+            // Display an error message if there was an error
+            resultSection.innerHTML = '<h2>An error occurred while fetching the data.</h2>';
+        });
 });
