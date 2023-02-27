4181684edf5f06777bbfa84d778ddf83b92ff1f0
Idriss <idrissbenguezzou@gmail.com>
2023-02-27 22:42:32 +0100

    add: usage function

--- wikibase/wikibase_admin/main.py
+++ wikibase/wikibase_admin/main.py
@@ -1,6 +1,8 @@
+from colorama import Fore, Style
 import eel
 import sys
 import os
+
 # add the parent directory of main.py to Python path to enable import modules from the wikibase package.
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -18,30 +20,45 @@ py_wb = PyWikibase(config_path=conf_path)
 # Set web files folder
 eel.init('web')
 
+
+def usage(program_name):
+    print(f"{Fore.GREEN}Usage: python {program_name} [OPTIONS]{Style.RESET_ALL}")
+    print("")
+    
+    print(Style.BRIGHT + Fore.BLUE + "Options:" + Style.RESET_ALL)
+    print(f"  {Fore.CYAN}-w, --web{Style.RESET_ALL}\tProcess data from web only")
+    print(f"  {Fore.CYAN}-a, --app{Style.RESET_ALL}\tProcess data from app only")
+    
+    print("")
+    sys.exit(1)
+
+
 @eel.expose
 def process_api_data():
     properties, data = data_culture_api.retrieve_data()
     inject_data(py_wb, data, properties)
 
+
 @eel.expose
 def process_csv_data(py_wb):
     properties, data = data_clic_csv.retrieve_data()
     inject_data(py_wb, data, properties)
+   
     
 @eel.expose
 def process_web_data(py_wb):
     properties, data = data_loire_web.retrieve_data()
     inject_data(py_wb, data, properties)
 
+
 def main(argv):
     
     if '-w' in argv or '--web' in argv:
         eel.start('index.html', mode='electron .')
     elif '-a' in argv or '--app' in argv:
-        eel.start('hello.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron.exe', '.'])
+        eel.start('index.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron.exe', '.'])
     else:
-        print('Usage: main.py [-w|--web] [-a|--app]')
-        sys.exit(1)
+        usage(argv[0])
 
 if __name__ == '__main__':
     main(sys.argv[1:])
