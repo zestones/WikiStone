ad6f0fd9be4bb1b6a66c00b0eea57fb60c57658d
Idriss <idrissbenguezzou@gmail.com>
2023-03-03 21:15:23 +0100

    feat: display nearby monument carousel

--- wikibase/wikibase_interface/static/js/item/item-proposition.js
+++ wikibase/wikibase_interface/static/js/item/item-proposition.js
@@ -2,7 +2,7 @@ import SPARQLQueryDispatcher from "../search/SPARQLQueryDispatcher.js";
 
 function searchNearbyMonuments(coordinates) {
     const position = "Point(" + coordinates.reverse().join(" ") + ")";
-    console.log(position);
+
     const sparqlQuery = `
         SELECT ?item ?itemLabel ?distance WHERE {
             SERVICE wikibase:around {
@@ -15,35 +15,68 @@ function searchNearbyMonuments(coordinates) {
             FILTER (?distance > 0)
         }
         ORDER BY (?distance)
-        LIMIT 3`
+        LIMIT 12`
         ;
 
     return sparqlQuery;
 }
 
 function displayResult(parsedData) {
-    console.log(parsedData)
-    // Générer le contenu HTML à partir des données
-    let html = '';
 
+    let html = '';
     for (const item in parsedData) {
+        let distance = parsedData[item].distance;
+
+        // convert distance to a number
+        distance = parseFloat(distance);
+        if (distance < 1) distance = (distance * 1000).toFixed(0) + ' m';
+        else distance = distance.toFixed(2) + ' km';
+
         html += `
-                <div class="item-card-proposition">
-                    <h4 onclick="window.location='/result?id=${item}'">${parsedData[item].label}</h4>
-                    <p>Distance: ${parsedData[item].distance} km</p>
-                `;
-        if (parsedData[item].description) {
-            html += `
-                    <p>${parsedData[item].description}</p>
-                    </div>
-                `;
-        }
-        else html += '</div>';
+            <div class="item-card-proposition">
+                <h4 onclick="window.location='/result?id=${item}'">${parsedData[item].label}</h4>
+                <p><strong>Distance: </strong>${distance}</p>
+            </div>
+        `;
     }
 
+
     // Insérer le contenu HTML dans la section
-    section.innerHTML = html;
+    section.innerHTML = `
+        <div class="carousel">
+            ${html}
+        </div>
+        <button class="carousel-prev"></button>
+        <button class="carousel-next"></button>
+    `;
+
+    // Initialize Slick.js carousel
+    $('.carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.carousel-prev'),
+        nextArrow: $('.carousel-next'),
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            }
+        ]
+    });
 }
+
 
 // Get the map element
 const mapElement = document.getElementById('map');
@@ -55,8 +88,22 @@ const sparqlQuery = searchNearbyMonuments(JSON.parse(location));
 const queryDispatcher = new SPARQLQueryDispatcher();
 queryDispatcher.query(sparqlQuery)
     .then((data) => {
-        displayResult(queryDispatcher.parse(data));
+        const parsedData = queryDispatcher.parse(data);
+        console.log(parsedData);
+
+        const sortedKeys = Object.keys(parsedData).sort((a, b) => {
+            const distanceA = parseFloat(parsedData[a].distance);
+            const distanceB = parseFloat(parsedData[b].distance);
+            return distanceA - distanceB;
+        });
+
+        const sortedData = {};
+        sortedKeys.forEach((key) => {
+            sortedData[key] = parsedData[key];
+        });
+
+        displayResult(sortedData);
     })
     .catch((error) => {
         console.log("error" + error)
-    })
+    });
